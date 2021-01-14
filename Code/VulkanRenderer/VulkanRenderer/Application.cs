b95f105c70be40fe1df3c42db0f8bea5bf0e0677
Faber Sanchez <faber.reach@gmail.com>
2021-01-14 01:05:39 -0500

    Add Application

---
+++ Code/VulkanRenderer/VulkanRenderer/Application.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulkanRenderer
+{
+    public unsafe sealed class Application : IDisposable
+    {
+        int F = 0;
+
+
+
+        public void Initialize()
+        {
+        }
+
+        public void MainLoop()
+        {
+        }
+
+
+        void InitializeGLFW()
+        {
+        }
+        bool InitializeVulkan()
+        {
+            return 0 == this.F;
+        }
+        void Cleanup()
+        {
+        }
+        void UpdateUniforms()
+        {
+        }
+        void DrawFrame()
+        {
+        }
+        void ResizeWindow(int windowWidth, int windowHeight)
+        {
+        }
+        void MouseMoved(float x, float y)
+        {
+        }
+        void MouseScrolled(float z)
+        {
+        }
+        void Keyboard(int key, int scancode, int action, int modifiers)
+        {
+        }
+
+        static void OnWindowResized(IntPtr* window, int width, int height)
+        {
+        }
+        static void OnMouseMoved(IntPtr* window, double x, double y)
+        {
+        }
+        static void OnMouseWheelScrolled(IntPtr* window, double x, double y)
+        {
+        }
+        static void OnKeyboard(IntPtr* window, int key, int scancode, int action, int modifiers)
+        {
+        }
+
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Code/VulkanRenderer/VulkanRenderer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VulkanRenderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Application app = new();
+            app.Initialize();
+            app.MainLoop();
+        }
+    }
+}
